2399db154b438064e6196491e1b682c07beb5f8a
贺涛涛 <he_tao_tao_email@163.com>
2019-05-09 11:50:13 +0800

    修改license

--- media/license/license.py
+++ media/license/license.py
@@ -9,16 +9,17 @@ class LicensePerssion(BasePermission):
     message = u"授权已过期,请联系管理人员更换授权文件"
 
     def has_permission(self, request, view):
-        time_ = datetime.strptime("2019-12-31", "%Y-%m-%d")
-        if datetime.now() >= time_:
-            with open("/opt/rh/httpd24/root/var/www/html/ResultManage/1.txt", "a") as f:
-                f.write(datetime.now().strftime("%Y-%m-%d"))
-                f.write("f \n")
-                f.close()
+        time_now = int(datetime.now().strftime("%Y%m%d"))
+        time_license = 21000101
+        if time_now >= time_license:
+            # with open("/opt/rh/httpd24/root/var/www/html/ResultManage/1.txt", "a") as f:
+            #     f.write(datetime.now().strftime("%Y-%m-%d"))
+            #     f.write("f \n")
+            #     f.close()
 
             return False
-        with open("/opt/rh/httpd24/root/var/www/html/ResultManage/1.txt", "a") as f:
-            f.write(datetime.now().strftime("%Y-%m-%d"))
-            f.write("t \n")
-            f.close()
+        # with open("/opt/rh/httpd24/root/var/www/html/ResultManage/1.txt", "a") as f:
+        #     f.write(datetime.now().strftime("%Y-%m-%d"))
+        #     f.write("t \n")
+        #     f.close()
         return True
